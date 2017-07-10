9f184272d57b698357e1e2080dffc56fe65f4eed
Joseph Geis <geis28@gmail.com>
2017-07-09 21:34:02 -0700

    Added "quiet" mode

--- rpassr.py
+++ rpassr.py
@@ -2,18 +2,19 @@ import base64, hashlib
 # print "Welcome to Restrictions Passcode Recovery\n----------"
 # print "H ~ Help\nR ~ Recover Passcode\nX ~ Exit"
 
-def recover():
+def recover(quiet):
   goal_key = bytes(input("What is RestrictionsPasswordKey? "),'utf8')
   salt = input("What is RestrictionsPasswordSalt? ")
   raw_code = 0
   while raw_code < 10000:
     code = ("0000" + str(raw_code))[-4:]
     trial = base64.b64encode(hashlib.pbkdf2_hmac('sha1', bytes(code,'utf8'), base64.b64decode(salt), 1000))
-    print("Code: %s, Produced: %s" % (code,trial))
+    if not quiet:
+      print("Code: %s, Produced: %s" % (code,trial))
     if trial == goal_key:
       print("FOUND PASSCODE! %s" % code)
       break
     else:
       raw_code += 1
 
-recover()
+recover(0)
